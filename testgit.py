e814eac372d72d8f5770ac20ffec624c803bb259
puckb <puck.bierwolf@student.hu.nl>
2025-11-10 16:08:26 +0100

    Tkinter oefenen 2e knop

--- testgit.py
+++ testgit.py
@@ -6,15 +6,32 @@ def onclick():
     outcome = f"square: of {base} = {square}"
     label['text'] = outcome
 
+def offclick():
+    getal1 = int(entry.get())
+    getal2 = int(entry2.get())
+    optelsom = getal1 * getal2
+    uitkomst = f"de vermenigvuldiging van {getal1} en {getal2} = {optelsom}"
+    label2['text'] = uitkomst
+
 root = Tk()
 
 label = Label(master=root, text="Maddox", height=2)
 label.pack()
 
+label2 = Label(master=root, text="Ayoub", height=4)
+label2.pack()
+
 button = Button(master=root, text="Click Me", command=onclick)
 button.pack(pady=10)
 
+button2 = Button(master=root, text="Klik op mij", command=offclick)
+button2.pack(pady=10)
+
 entry = Entry(master=root)
 entry.pack(padx=10, pady=10)
+
+entry2 = Entry(master=root)
+entry2.pack(padx=10, pady=10)
+
 
 root.mainloop()
