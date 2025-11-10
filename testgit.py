329fce23b29f17ec2be5d7de18a7d2a4bc784548
puckb <puck.bierwolf@student.hu.nl>
2025-11-10 15:51:41 +0100

    Tkinter

--- testgit.py
+++ testgit.py
@@ -1,3 +1,14 @@
-print("hello")
+from tkinter import *
 
-json
+root = Tk()
+
+label = Label(master=root, text="Maddox", height=2)
+label.pack()
+
+button = Button(master=root, text="Click Me")
+button.pack(pady=10)
+
+entry = Entry(master=root)
+entry.pack(padx=10, pady=10)
+
+root.mainloop()
