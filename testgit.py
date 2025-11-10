570502962064b4b6a3084b20bb385efdf5418dc0
puckb <puck.bierwolf@student.hu.nl>
2025-11-10 15:55:43 +0100

    Tkinter oefenen

--- testgit.py
+++ testgit.py
@@ -1,11 +1,17 @@
 from tkinter import *
 
+def onclick():
+    base = int(entry.get())
+    square = base**2
+    outcome = f"square: of {base} = {square}"
+    label['text'] = outcome
+
 root = Tk()
 
 label = Label(master=root, text="Maddox", height=2)
 label.pack()
 
-button = Button(master=root, text="Click Me")
+button = Button(master=root, text="Click Me", command=onclick)
 button.pack(pady=10)
 
 entry = Entry(master=root)
